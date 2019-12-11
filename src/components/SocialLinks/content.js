de474b2ba90bd695c2858415c8d41dcfed46e741
Diogo Cabral <drcabral@thoughtworks.com>
2019-12-11 02:03:45 -0300

    Adding social links to sidebar

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/drcabral",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/DrCabrales",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/drcabral/",
+  },
+]
+
+export default links
