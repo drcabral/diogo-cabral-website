29de40ee8339ab731c99440d1d05584034f45c79
Diogo Cabral <drcabral@thoughtworks.com>
2019-12-17 14:52:41 -0300

    Creating sitemap and robots file

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,7 @@ module.exports = {
         icon: `src/images/favicon.png`,
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
